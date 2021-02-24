963f62138516ed4b2fd17e8f192c82550d9aea5a
jhc-shou <deanny.chung@johnson-hc.com>
2021-02-24 09:48:29 +0900

    [Mod] modified project files

--- getRakutenReview.py
+++ getRakutenReview.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# get commends from Amazon
+# get reviews from Rakuten ichiba
 import time
 import json
 import csv
@@ -27,19 +27,22 @@ driver = driver = webdriver.Chrome(chrome_options=options,
                                    executable_path=ChromeDriverManager().install())
 
 # extract html to dict
+
+
 def getDictComm():
     html = driver.page_source.replace('\n', '')
     return e.extract(html)
 
 # goto next page
+
+
 def NextPage():
     try:
         # wait page load
         time.sleep(3)
-        driver.execute_script("return arguments[0].scrollIntoView(true);", WebDriverWait(
-            driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//li[@class='a-last']/a"))))
-        driver.find_element_by_xpath(
-            "//li[@class='a-last']/a").click()
+        element = WebDriverWait(
+            driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, '次の15件 >>')))
+        element.click()
         print("Navigating to Next Page")
         return True
     except (TimeoutException, WebDriverException) as e:
@@ -47,26 +50,38 @@ def NextPage():
         return False
 
 # write dict to scv file
+
+
 def writeToCSV(dictComm, writer):
-    for r in dictComm['reviews']:
+    for (r, rr) in zip(dictComm['reviews'], dictComm['reviewers']):
         r["product"] = dictComm["product_title"]
-        r['verified'] = 'Yes' if r['verified'] else 'No'
-        r['rating'] = r['rating'].split(
-            ' out of')[0] if r['rating'] else 'N/A'
-        r['images'] = " ".join(
-            r['images']) if r['images'] else 'N/A'
-        r['date'] = dateparser.parse(
-            r['date'].split('on ')[-1]).strftime('%d %b %Y')
+        r['rating'] = r['rating'] if r['rating'] else 'N/A'
+        r['author'] = rr['name']
         writer.writerow(r)
 
 # product_data = []
+
+
 def getRakutenReview():
     with open("urls.txt", 'r') as urllist:
         for url in urllist.readlines():
             driver.get(url)
             html = driver.page_source.replace('\n', '')
             dictComm = e.extract(html)
-            print(dictComm)
+            if dictComm:
+                productTittle = re.findall(
+                    r'[^\*"/:?\\|<>]', dictComm["product_title"].replace(' ', '_'), re.S)
+                csvFileName = "".join(productTittle) + '.csv'
+                with open('comm/'+csvFileName, 'w', encoding='UTF-8', errors='ignore') as outfile:
+                    writer = csv.DictWriter(outfile, fieldnames=[
+                                            "title", "content", "date", "variant", "images", "verified", "author", "rating", "product"], quoting=csv.QUOTE_ALL)
+                    writer.writeheader()
+                    writeToCSV(dictComm, writer)
+                    while True:
+                        if NextPage():
+                            writeToCSV(getDictComm(), writer)
+                        else:
+                            break
 
     driver.close()
 
