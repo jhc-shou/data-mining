67fba018cda5c9ed36bd7fc85b2d84c8f71574b9
jhc-shou <deanny.chung@johnson-hc.com>
2021-03-16 13:14:07 +0900

    [Mod] modified private method

--- eCSiteReviewCrawler.py
+++ eCSiteReviewCrawler.py
@@ -14,42 +14,39 @@ from selenium.common.exceptions import TimeoutException, WebDriverException
 
 from impReviewProperty import ReviewProperty
 
-reviewProperty = ReviewProperty()
-
-options = webdriver.ChromeOptions()
-options.add_argument("start-maximized")
-options.add_argument("disable-infobars")
-options.add_argument("--disable-extensions")
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-driver = driver = webdriver.Chrome(chrome_options=options,
-                                   executable_path=ChromeDriverManager().install())
-
 
 class ReviewAPI:
-
     def __init__(self):
-        pass
+        self.__reviewProperty = ReviewProperty()
+
+        self.__options = webdriver.ChromeOptions()
+        __options.add_argument("start-maximized")
+        __options.add_argument("disable-infobars")
+        __options.add_argument("--disable-extensions")
+        __options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        self.__driver = driver = webdriver.Chrome(chrome_options=__options,
+                                        executable_path=ChromeDriverManager().install())
 
     # extract html to dict
     # Create an Extractor by reading from the YAML file
-    def extractUrl(self):
-        e = Extractor.from_yaml_file(reviewProperty.getSiteSelector)
-        html = driver.page_source.replace('\n', '')
-        reviewProperty.setDictComm(e.extract(html))
+    def __extractUrl(self):
+        e = Extractor.from_yaml_file(self.__reviewProperty.getSiteSelector)
+        html = self.__driver.page_source.replace('\n', '')
+        self.__reviewProperty.setDictComm(e.extract(html))
 
     # goto next page
-    def NextPage(self):
+    def __NextPage(self):
         try:
             # wait page load
             time.sleep(3)
-            if reviewProperty.getECSite == 'rakuten':
+            if self.__reviewProperty.getECSite == 'rakuten':
                 element = WebDriverWait(
-                    driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, '次の15件 >>')))
+                    self.__driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, '次の15件 >>')))
                 element.click()
-            elif reviewProperty.getECSite == 'amazon':
-                driver.execute_script("return arguments[0].scrollIntoView(true);", WebDriverWait(
-                    driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//li[@class='a-last']/a"))))
-                driver.find_element_by_xpath(
+            elif self.__reviewProperty.getECSite == 'amazon':
+                self.__driver.execute_script("return arguments[0].scrollIntoView(true);", WebDriverWait(
+                    self.__driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//li[@class='a-last']/a"))))
+                self.__driver.find_element_by_xpath(
                     "//li[@class='a-last']/a").click()
             print("Navigating to Next Page")
             return True
@@ -58,16 +55,16 @@ class ReviewAPI:
             return False
 
     # write dict to scv file
-    def writeToCSV(self, writer):
-        if reviewProperty.getECSite == 'rakuten':
-            for (r, rr) in zip(reviewProperty.getDictComm['reviews'], reviewProperty.getDictComm['reviewers']):
-                r["product"] = reviewProperty.getDictComm["product_title"]
+    def __writeToCSV(self, writer):
+        if self.__reviewProperty.getECSite == 'rakuten':
+            for (r, rr) in zip(self.__reviewProperty.getDictComm['reviews'], self.__reviewProperty.getDictComm['reviewers']):
+                r["product"] = self.__reviewProperty.getDictComm["product_title"]
                 r['rating'] = r['rating'] if r['rating'] else 'N/A'
                 r['author'] = rr['name']
                 writer.writerow(r)
-        elif reviewProperty.getECSite == 'amazon':
-            for r in reviewProperty.getDictComm['reviews']:
-                r["product"] = reviewProperty.getDictComm["product_title"]
+        elif self.__reviewProperty.getECSite == 'amazon':
+            for r in self.__reviewProperty.getDictComm['reviews']:
+                r["product"] = self.__reviewProperty.getDictComm["product_title"]
                 r['verified'] = 'Yes' if r['verified'] else 'No'
                 r['rating'] = r['rating'].split(' out of')[0] if r['rating'] else 'N/A'
                 r['images'] = " ".join(r['images']) if r['images'] else 'N/A'
@@ -77,45 +74,39 @@ class ReviewAPI:
                 day = r['date'].split('年')[1].split('月')[1].split('日')[0]
                 r['date'] = '-'.join((year,month,day))
                 writer.writerow(r)
-        
 
+    # product_data = []
+    def reviewCrawler(self):
+        api = ReviewAPI()
+        with open("urls.txt", 'r') as urllist:
+            for url in urllist.readlines():
 
-# product_data = []
-def reviewCrawler():
-    api = ReviewAPI()
-    with open("urls.txt", 'r') as urllist:
-        for url in urllist.readlines():
+                # check EC site source
+                if url.find('rakuten') != -1:
+                    self.__reviewProperty.setECSite("rakuten")
+                elif url.find('amazon') != -1:
+                    self.__reviewProperty.setECSite('amazon')
+                else:
+                    print('wrong url: ', url,'\n')
+                    continue
 
-            # check EC site source
-            if url.find('rakuten') != -1:
-                reviewProperty.setECSite("rakuten")
-            elif url.find('amazon') != -1:
-                reviewProperty.setECSite('amazon')
-            else:
-                print('wrong url: ', url,'\n')
-                continue
-
-            driver.get(url)
-            api.extractUrl()
-            if reviewProperty.getDictComm:
-                productTittle = re.findall(
-                    r'[^\*"/:?\\|<>]', reviewProperty.getDictComm["product_title"].replace(' ', '_'), re.S)
-                csvFileName = "".join(productTittle) + '.csv'
-                try:
-                    with open('comm/'+csvFileName, 'w', encoding='UTF-8', errors='ignore') as outfile:
-                        writer = csv.DictWriter(outfile, fieldnames=["title", "content", "date", "variant",
-                                                                    "images", "verified", "author", "rating", "product"], quoting=csv.QUOTE_ALL)
-                        writer.writeheader()
-                        api.writeToCSV(writer)
-                        while True:
-                            if api.NextPage():
-                                api.writeToCSV(writer)
-                            else:
-                                break
-                except IOError as ioe:
-                    print('file error:', ioe)
-    driver.close()
-
-
-if __name__ == '__main__':
-    reviewCrawler()
+                self.__driver.get(url)
+                api.extractUrl()
+                if self.__reviewProperty.getDictComm:
+                    productTittle = re.findall(
+                        r'[^\*"/:?\\|<>]', self.__reviewProperty.getDictComm["product_title"].replace(' ', '_'), re.S)
+                    csvFileName = "".join(productTittle) + '.csv'
+                    try:
+                        with open('comm/'+csvFileName, 'w', encoding='UTF-8', errors='ignore') as outfile:
+                            writer = csv.DictWriter(outfile, fieldnames=["title", "content", "date", "variant",
+                                                                        "images", "verified", "author", "rating", "product"], quoting=csv.QUOTE_ALL)
+                            writer.writeheader()
+                            api.writeToCSV(writer)
+                            while True:
+                                if api.NextPage():
+                                    api.writeToCSV(writer)
+                                else:
+                                    break
+                    except IOError as ioe:
+                        print('file error:', ioe)
+        self.__driver.close()
--- impReviewProperty.py
+++ impReviewProperty.py
@@ -1,30 +1,30 @@
 class ReviewProperty:
     def __init__(self):
-        self.ecSite = ''
-        self.dictComm = dict()
+        self.__ecSite = ''
+        self.__dictComm = dict()
 
     # Set EC sit
     def setECSite(self, ecSite):
-        self.ecSite = ecSite
+        self.__ecSite = ecSite
 
     # Get EC site
     @property
     def getECSite(self):
-        return self.ecSite
+        return self.__ecSite
 
     # Get selector for EC site
     @property
     def getSiteSelector(self):
-        if self.ecSite == 'rakuten':
+        if self.__ecSite == 'rakuten':
             return 'rakutenSelectors.yml'
-        elif self.ecSite == 'amazon':
+        elif self.__ecSite == 'amazon':
             return 'amazonSelectors.yml'
 
     # Set dictionary of review from EC site
     def setDictComm(self, dictComm):
-        self.dictComm = dictComm
+        self.__dictComm = dictComm
 
     # Get dictionary of review from EC site
     @property
     def getDictComm(self):
-        return self.dictComm
+        return self.__dictComm
